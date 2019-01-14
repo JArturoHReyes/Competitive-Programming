dfa72b9b2215ba4727b7f52d20fa7435eb2aa824
JArturoHReyes <35275137+JArturoHReyes@users.noreply.github.com>
2019-01-14 13:54:58 -0600

    Create E - Exact Sum.cpp

---
+++ ACM ICPC/Region Mexico/2017/Segunda Fecha/E - Exact Sum.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int cases;
+    cin >> cases;
+    while(cases--)
+    {
+      cout << "Yes." << endl;
+    }
+    return 0;
+}
