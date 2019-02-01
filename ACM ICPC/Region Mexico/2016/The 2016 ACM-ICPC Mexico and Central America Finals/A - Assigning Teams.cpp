8a2ea8ffff088b8cbda344785aa59a5387bd5078
JArturoHReyes <35275137+JArturoHReyes@users.noreply.github.com>
2019-01-31 21:15:57 -0600

    Create A - Assigning Teams.cpp

---
+++ ACM ICPC/Region Mexico/2016/The 2016 ACM-ICPC Mexico and Central America Finals/A - Assigning Teams.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main()
+{
+     
+    int a , b , c , d;
+    
+    while(cin >> a >> b >> c >> d)
+    {
+        cout << abs((a+d)-(b+c)) <<e ndl;
+    }
+ 
+    return 0;
+}
