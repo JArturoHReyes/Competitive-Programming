9996a1e1efd815573f3b67a9c2cfa7fc9283b51e
JArturoHReyes <35275137+JArturoHReyes@users.noreply.github.com>
2019-04-14 00:17:19 -0500

    Create J - Jacksonville Police Departament.cpp

---
+++ ACM ICPC/Region Mexico/2015/Premio Fase I/J - Jacksonville Police Departament.cpp
@@ -0,0 +1,108 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define endl '\n'
+using namespace std;
+
+int k , n , f;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	if(fopen("InputArturoJ.txt" , "r")) freopen("InputArturoJ.txt" , "r" , stdin);
+
+	while(cin >> k >> n && k && n)
+    {
+        bitset < 260 > d;
+
+        while(n--)
+        {
+            cin >> f;
+
+            for(int i = f; i <= k; i += f)
+            {
+                d[i] = 1;
+            }
+        }
+
+        cout << d.count() << endl;
+    }
+
+
+	return 0;
+}
