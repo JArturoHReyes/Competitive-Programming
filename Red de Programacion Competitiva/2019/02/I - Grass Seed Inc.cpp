7be06b6fb3a29f3fcd951bbcdf1f3573771f077c
JArturoHReyes <35275137+JArturoHReyes@users.noreply.github.com>
2019-04-22 21:47:27 -0500

    Create I - Grass Seed Inc.cpp

---
+++ Red de Programacion Competitiva/2019/02/I - Grass Seed Inc.cpp
@@ -0,0 +1,104 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define endl '\n'
+using namespace std;
+
+long double m , sum = 0.0 , a , b;
+
+int n;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cout << setprecision(10) << fixed;
+	
+	if(fopen("InputArturoI.txt" , "r")) freopen("InputArturoI.txt" , "r" , stdin);
+
+	cin >> m >> n;
+
+	while(n--)
+	{
+		cin >> a >> b;
+
+		sum += (a * b);
+	}
+
+	cout << sum * m << endl;
+	
+	return 0;
+}
