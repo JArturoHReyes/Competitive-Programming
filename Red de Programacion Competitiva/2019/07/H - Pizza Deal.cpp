0584c3a32e6643054ad953821afc69c7576c5aa0
JArturoHReyes <35275137+JArturoHReyes@users.noreply.github.com>
2019-09-07 20:25:56 -0500

    Create H - Pizza Deal.cpp

---
+++ Red de Programacion Competitiva/2019/07/H - Pizza Deal.cpp
@@ -0,0 +1,109 @@
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#define endl '\n'
+using namespace std;
+
+long double a , b , c , d;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	if(fopen("InputArturoA.txt" , "r")) freopen("InputArturoA.txt" , "r" , stdin);
+	
+	long double pi = acos(-1.0);
+
+	cin >> a >> b >> c >> d;
+
+	long double area = pi * c * c;
+
+	long double circle = area / d;
+
+	long double square = a / b;
+
+	if(circle < square)
+	{
+		cout << "Slice of pizza" << endl;
+	}
+	else
+	{
+		cout << "Whole pizza" << endl;
+	}
+	
+	return 0;
+}
